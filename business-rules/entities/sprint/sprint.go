dba9b1eb977b4e2ae0e0717ab8c4480198282674
Manfred Touron <m@42.am>
2016-06-21 16:35:03 +0200

    Add sprint

---
+++ business-rules/entities/sprint/sprint.go
@@ -0,0 +1,81 @@
+package sprint
+
+import (
+	"time"
+
+	"github.com/moul/go-clean-architecture/business-rules/entities/issues"
+)
+
+// Sprint represents an sprint.
+type Sprint struct {
+	id                int
+	status            string
+	createdAt         time.Time
+	expectedClosedAt  time.Time
+	effectiveClosedAt time.Time
+	issues            []*issue.Issue
+}
+
+// New returns an instanciated instance of Sprint.
+func New() *Sprint {
+	return &Sprint{
+		issues:    make([]*issue.Issue, 0),
+		createdAt: time.Now(),
+	}
+}
+
+// GetID returns the ID of the sprint.
+func (i *Sprint) GetID() int { return i.id }
+
+// GetStatus returns the status of the sprint.
+func (i *Sprint) GetStatus() string { return i.status }
+
+// GetCreatedAt returns the creation date of the sprint.
+func (i *Sprint) GetCreatedAt() time.Time { return i.createdAt }
+
+// GetExpectedClosedAt returns the finish date of the sprint.
+func (i *Sprint) GetExpectedClosedAt() time.Time { return i.expectedClosedAt }
+
+// GetEffectiveClosedAt returns the finish date of the sprint.
+func (i *Sprint) GetEffectiveClosedAt() time.Time { return i.effectiveClosedAt }
+
+// AddIssue adds an issue to the sprint.
+func (i *Sprint) AddIssue(issue *issue.Issue) {
+	i.issues = append(i.issues, issue)
+}
+
+// GetIssues returns the issues of the sprint.
+func (i *Sprint) GetIssues() []*issue.Issue {
+	return i.issues
+}
+
+// GetIssuesCount returns the count of issues in the sprint.
+func (i *Sprint) GetIssuesCount() int {
+	return len(i.issues)
+}
+
+// IsClosed returns true if the sprint status is "CLOSED".
+func (i *Sprint) IsClosed() bool { return i.GetStatus() == Closed }
+
+// Close closes an open sprint
+func (i *Sprint) Close() error {
+	if i.IsClosed() {
+		return &AlreadyClosedError{}
+	}
+
+	for idx := len(i.issues) - 1; idx >= 0; idx-- {
+		issue := i.issues[idx]
+
+		if issue.IsDone() {
+			if err := issue.Close(); err != nil {
+				return err
+			}
+		} else {
+			i.issues = append(i.issues[:idx], i.issues[idx+1:]...)
+		}
+	}
+
+	i.effectiveClosedAt = time.Now()
+	i.status = Closed
+	return nil
+}
---
+++ business-rules/entities/sprint/status.go
@@ -0,0 +1,9 @@
+package sprint
+
+var (
+	// Open is the status of a sprint that is still open
+	Open = "OPEN"
+
+	// Close is the status of an sprint that were closed
+	Closed = "CLOSED"
+)
