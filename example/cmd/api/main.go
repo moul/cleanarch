37e1cede335d883422f686c10bd533d58fe0fd2d
Manfred Touron <m@42.am>
2016-09-21 08:45:48 +0200

    Configure the gateway dynamically from the main

--- example/cmd/api/main.go
+++ example/cmd/api/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"github.com/moul/cleanarch/example/app/controllers/api"
 	"github.com/moul/cleanarch/example/app/repos/sprints/gorm"
+	"github.com/moul/cleanarch/example/app/repos/sprints/mem"
+	"github.com/moul/cleanarch/example/bizrules/gateways"
 	"github.com/moul/cleanarch/example/bizrules/usecases/add_sprint"
 	"github.com/moul/cleanarch/example/bizrules/usecases/add_sprint/dto"
 	"github.com/moul/cleanarch/example/bizrules/usecases/get_sprint"
@@ -16,15 +20,20 @@ import (
 )
 
 func main() {
-	// Initialize database
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
 	// Setup gateways
-	sprintsGw := sprintsgorm.New(db)
+	var sprintsGw gateways.Sprints
+	if len(os.Args) > 1 && os.Args[1] == "--mem" {
+		// configure a memory-based sprints gateway
+		sprintsGw = sprintsmem.New()
+	} else {
+		// configure a sqlite-based sprints gateway
+		db, err := gorm.Open("sqlite3", "test.db")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+		sprintsGw = sprintsgorm.New(db)
+	}
 
 	// Setup usecases
 	getSprint := getsprint.New(sprintsGw, getsprintdto.ResponseAssembler{})
